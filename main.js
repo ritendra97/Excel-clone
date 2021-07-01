07cf9abc2aca383b8543d4b2caa26698adb39349
Ritendra Pratap Singh <53062734+ritendra97@users.noreply.github.com>
2021-07-01 08:53:51 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+const electron = require("electron");
+const ejs = require("ejs-electron");
+ejs.data({
+    title: "My Excel",
+    rows: 100,
+    cols: 26
+})
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+async function createWindow() {
+    const win = new BrowserWindow({
+        // provides node to electron app
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    await win.loadFile("index.ejs");
+    win.maximize();
+    win.webContents.openDevTools();
+}
+app.whenReady().then(createWindow);
